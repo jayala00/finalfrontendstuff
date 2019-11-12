2769449ac686f2f57ee58667f5c950117a85a153
Lawton Stalvey <lstalve@g.clemson.edu>
2019-11-11 21:05:50 -0500

    temp

--- src/app/createdriver/createdriver.component.ts
+++ src/app/createdriver/createdriver.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import { Driver } from '../driver';
+import { DriverserviceService } from '../service/driverservice.service';
 
 @Component({
   selector: 'app-createdriver',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreatedriverComponent implements OnInit {
 
-  constructor() { }
+  driver: Driver = new Driver();
+
+  constructor(private router:Router, private driverService: DriverserviceService) { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    console.log(this.driver.id);
+    console.log(this.driver.driverID);
+    console.log(this.driver.firstname);
+    console.log(this.driver.lastname);
+    console.log(this.driver.email);
+    console.log(this.driver.password);
+    console.log(this.driver.address);
+    
+    this.driverService.createDriver(this.driver)
+    .subscribe( data => {
+      alert('Driver created successfully.');
+    })
+    this.router.navigate(['/admin'])
   }
 
 }
---
+++ src/app/service/driverservice.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import {Driver} from '../driver';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class DriverserviceService {
+
+  constructor(private http:HttpClient) { }
+
+  private driverUrl = 'http://localhost:8080';
+
+  public createDriver(driver) {
+    return this.http.post<Driver>(this.driverUrl + '/adddrivers', driver);
+  }
+
+  public getUsers() {
+    return this.http.get<Driver[]>(this.driverUrl + '/getdrivers');
+  }
+
+  public deleteUser(driver) {
+    return this.http.delete(this.driverUrl + "/"+ driver.id);
+  }
+}
+
